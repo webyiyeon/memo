8c2578ae18a69cda6dfe05cda0dd3b6112775292
webyiyeon <yiyeon79@gmail.com>
2022-05-06 13:57:42 +0900

    4th commit

--- js/memo_script.js
+++ js/memo_script.js
@@ -1,48 +1,32 @@
-// let view = querySelectorAll("")
-// let isOpen = false;
-
-// for(let i=0; i<view.length; i++){
-//     view[i].addEventListener("click", function(){
-//         if(isOpen == false){
-//             document.querySelectorAll(".insideMenus")[i].style.display="inline-block";
-//              isOpen = true;
-//          }else{
-//             document.querySelectorAll(".insideMenus")[i].style.display="none";
-//              isOpen = false;
-//          }
-//     });
-// }
-
-
-// $('.menuBtns').click(function(){
-//     let inside = $(this).siblings('.insideMenus').css('display');
-//     if(inside == 'none'){
-//         $('.insideMenus').slideUp(300);
-//         $(this).siblings('.insideMenus').slideDown(300);
-//     }else{
-//         $('.insideMenus').slideUp(300);
-//         // $(this).siblings('.insideMenus').slideDown();
-//     }
-
-//     return false;
-// })
-
 let menuBtns = document.querySelectorAll('.menuBtns');
 let insideMenus = document.querySelectorAll('.insideMenus')
 let isOpen = false;
 
 
-if(isOpen==false){
-    for(let i=0; i<menuBtns.length; i++){
-        menuBtns[i].addEventListener("click", function(){
+for(let i=0; i<menuBtns.length; i++){
+    menuBtns[i].addEventListener("click", function(){
+        if(isOpen==false){
             for(let j=0; j<insideMenus.length; j++){
                 insideMenus[j].style.display = "none"
             }
             insideMenus[i].style.display = "block"
-        })
-        isOpen = true;
-    }
-}
+            isOpen=true;
+        }else{
+            if(insideMenus[i].style.display=="block"){
+                for(let j=0; j<insideMenus.length; j++){
+                    insideMenus[j].style.display = "none"
+                }
+                isOpen=false;
+            }else{
+                for(let j=0; j<insideMenus.length; j++){
+                    insideMenus[j].style.display = "none"
+                }
+                insideMenus[i].style.display="block"
+                isOpen=true;
+            }  
+        }
+    })
+}       
 
 document.addEventListener("click",function(e){
     if(e.target.className != "menuBtns"){
